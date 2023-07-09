976efcb9b8475dd0bbec47f7d2bb7f57112b0a3b
Adan Nogueira <adan.nogueira@gmail.com>
2023-07-08 21:05:48 -0300

    chore: 🙈 update environment information

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,12 @@ require('dotenv').config()
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  env: {
+    API_URL: process.env.APP_API_URL
+  },
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: process.env.BASE_URL
+    baseUrl: process.env.APP_WEB_URL
   },
 });
